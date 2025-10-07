abb6af80f0537811f9247facd04c328715334a1a
miladjalilvand <miladjalilvand3666@gmail.com>
2025-10-07 13:49:01 +0330

    clear test Modules

--- Modules/Orders/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Orders\Http\Controllers\OrdersController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('orders', OrdersController::class)->names('orders');
-});
--- Modules/Payments/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Payments\Http\Controllers\PaymentsController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('payments', PaymentsController::class)->names('payments');
-});
