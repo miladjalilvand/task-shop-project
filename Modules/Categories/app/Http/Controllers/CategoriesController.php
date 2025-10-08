94cef992c7f6b67554affb35b99122169cf90670
miladjalilvand <miladjalilvand3666@gmail.com>
2025-10-08 15:38:00 +0330

    update Categorycontroller.index controller

--- Modules/Categories/app/Http/Controllers/CategoriesController.php
+++ Modules/Categories/app/Http/Controllers/CategoriesController.php
@@ -16,7 +16,9 @@ class CategoriesController extends Controller
      */
     public function index()
     {
-        return view('categories::index');
+        $categories = Category::all();
+
+        return view('categories::index',compact('categories'));
     }
 
     /**
